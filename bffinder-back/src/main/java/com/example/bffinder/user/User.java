0af016ca683de0c3733936c1b76a56579f605e79
kroad0129 <kroad0129@naver.com>
2025-07-09 16:31:12 +0900

    수정

--- bffinder-back/src/main/java/com/example/bffinder/user/User.java
+++ bffinder-back/src/main/java/com/example/bffinder/user/User.java
@@ -27,9 +27,11 @@ public class User {
     private String email;
 
     @Column(nullable = false)
+    @Builder.Default
     private String role = "USER"; // 기본값 USER
 
     @Column(nullable = false)
+    @Builder.Default
     private boolean isEmailVerified = false; // 기본값 false
 
     // 이메일 인증용 토큰(랜덤 UUID 등)
